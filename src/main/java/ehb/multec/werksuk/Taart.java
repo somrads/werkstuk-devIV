805501f61c20c58b8673bc3f3fff86a68aa68fb4
manishnepali <manish.nepali@student.ehb.be>
2021-05-18 18:07:58 +0200

    test

--- src/main/java/ehb/multec/werksuk/Taart.java
+++ src/main/java/ehb/multec/werksuk/Taart.java
@@ -11,3 +11,7 @@ public class Taart {
         return taart;
     }
 }
+
+
+/*singleton Thread Safe Singleton
+ https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples */
