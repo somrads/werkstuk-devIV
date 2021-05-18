7d6f3016f51b626cd97e9b7c69048e1e0509c8c0
somrads <43730541+somrads@users.noreply.github.com>
2021-05-18 21:00:50 +0200

    singleton correcties

--- src/main/java/ehb/multec/werksuk/Taart.java
+++ src/main/java/ehb/multec/werksuk/Taart.java
@@ -1,10 +1,11 @@
 package ehb.multec.werksuk;
 
-
 public class Taart {
-    private static Taart taart = new Taart();
+    private static Taart taart = null;
+
     private Taart(){}
-    public static synchronized Taart getTaart(){
+
+    public static Taart getInstance(){
         if(taart == null){
             taart = new Taart();
         }
