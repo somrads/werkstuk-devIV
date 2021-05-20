f00dbb3f1936e6344d40d3bb78ccaabbf59956bc
manishnepali <manish.nepali@student.ehb.be>
2021-05-21 00:02:13 +0200

    Singleton - implementation slide 26

--- src/main/java/ehb/multec/werksuk/Bakkerij.java
+++ src/main/java/ehb/multec/werksuk/Bakkerij.java
@@ -1,33 +1,22 @@
 package ehb.multec.werksuk;
 /* singleton we maken een bakkerij*/
-public class Bakkerij {
 
+public class Bakkerij
+{
 
-    private static Bakkerij bakkerij = null;
+    private Bakkerij() {}
 
-    private Bakkerij(){}
-
-    static boolean firsThread = true;
-
-    public static Bakkerij getInstance() throws InterruptedException {
-        if(bakkerij == null){
-
-            if(firsThread){
-
-                firsThread = false;
-                Thread.sleep(1000);
-            }
-
-            bakkerij = new Bakkerij();
-        }
-
-        synchronized (Bakkerij.class){
-            if(bakkerij == null){
-                bakkerij = new Bakkerij();
-            }
-        }
-        return bakkerij;
+    private static class BakkerijHolder
+    {
+        private static final Bakkerij INSTANCE = new Bakkerij();
+    }
+    public static Bakkerij getInstance()
+    {
+        return BakkerijHolder.INSTANCE;
     }
 }
+
+
+
 
 
