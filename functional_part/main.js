d3a1079f5c072a4d67f1b407532277b8e6bdc279
somrads <43730541+somrads@users.noreply.github.com>
2021-05-23 00:49:16 +0200

    verder aan function quick sort werken

--- functional_part/main.js
+++ functional_part/main.js
@@ -19,22 +19,7 @@ console.log(cakeWithTopping);
 
 
 function quickSort(array){
-    if(array.length == 1){
-        return array;
-    }
-
-    const pivot = array[array.length - 1]
-    const leftArr = [];
-    const rightArr = [];
-    for(let i = 0; i< array.length - 1; i++){
-        if(array[i] < pivot) {
-            leftArr.push(array[i]);
-        } else {
-            rightArr.push(array[i]);
-        }
-    }
-
-    return array;
+    
 }
 
 
