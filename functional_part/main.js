ed229a02909259d645acae81ae3a8e631367895a
manishnepali <manish.nepali@student.ehb.be>
2021-05-22 21:55:14 +0200

    funtion gemaakt voor toppings

--- functional_part/main.js
+++ functional_part/main.js
@@ -2,9 +2,21 @@ console.log("loaded");
 
 //decorater patteren, add diffrent toppings on cake = new cake
 
-const cake = "cake";
+const cake = ["cake"];
 const toppings = ["chocolate", "whip cream", "strawberry"]
 
+function cakesWeGot(topping){
+    toppings.forEach((topping)=>{
+        htmlString = "";
+    console.log(topping);
+    
+    htmlString = `<li>${topping} ${cake}</li> `
+    document.getElementById("cakes").innerHTML += htmlString
+    })
+
+}
+cakesWeGot();
+/*
 toppings.forEach((toppings)=> {
     htmlString = "";
     console.log(toppings, cake);
@@ -12,9 +24,9 @@ toppings.forEach((toppings)=> {
     htmlString = `<li>${toppings} ${cake}</li> `
     document.getElementById("cakes").innerHTML += htmlString
 });
+*/
 
-
-//template patteren = stappen plan
+//template patteren = stappen plan to make cupCakes or plainclake
 
 
 /*
