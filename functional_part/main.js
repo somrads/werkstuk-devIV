85741f7a208814fd6638a4684a9106332956999f
somrads <43730541+somrads@users.noreply.github.com>
2021-05-23 00:49:00 +0200

    function quicksort bijgevoegd

--- functional_part/main.js
+++ functional_part/main.js
@@ -17,12 +17,24 @@ function cakeMaker2(cake){
 const cakeWithTopping = toppings.map(cakeMaker2("cake"))
 console.log(cakeWithTopping);
 
-  
-  
-  
 
 function quickSort(array){
+    if(array.length == 1){
+        return array;
+    }
 
+    const pivot = array[array.length - 1]
+    const leftArr = [];
+    const rightArr = [];
+    for(let i = 0; i< array.length - 1; i++){
+        if(array[i] < pivot) {
+            leftArr.push(array[i]);
+        } else {
+            rightArr.push(array[i]);
+        }
+    }
+
+    return array;
 }
 
 
