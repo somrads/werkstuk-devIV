f4103f993ec5d72cea83d8d4f34978f9fc903326
manishnepali <manish.nepali@student.ehb.be>
2021-05-23 12:01:36 +0200

    test singleton

---
+++ src/test/java/ehb/multec/werksuk/BakeryTest.java
@@ -0,0 +1,12 @@
+package ehb.multec.werksuk;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BakeryTest {
+
+    @Test
+    void getInstance() {
+
+    }
+}
