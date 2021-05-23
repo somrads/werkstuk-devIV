5edf28cb55235fc73f82e10dc8ddc8581d164691
manishnepali <manish.nepali@student.ehb.be>
2021-05-23 12:04:17 +0200

    Merge branch 'develop' of https://github.com/somrads/werkstuk-devIV into develop

--- functional_part/main.js
+++ functional_part/main.js
@@ -1,87 +1,45 @@
-console.log("loaded");
-
 //decorater pattern, add different toppings on cake = new cake
 
 const toppings = ["chocolate", "whip cream", "strawberry", "carrot", "oreo"]
 
-function cakeMaker(topping, cake){
+function cakeMaker(topping, cake) {
     return `${topping} ${cake}`;
 }
 
-function cakeMaker2(cake){
-    return function (topping){
+function cakeMaker2(cake) {
+    return function (topping) {
         return `${topping} ${cake}`;
     }
 }
 
-
-function quickSort(array){
-    if(array.length == 1){
-        return array;
-    }
-const newCake2 = toppings.map(cakeMaker2("cake"));
-document.getElementById("cakes").innerHTML += `<p>${newCake2}</p>`;
-console.log(newCake2);
+const newCake = toppings.map(cakeMaker2("cake"));
+document.getElementById("cakes").innerHTML += `<p>${newCake}</p>`;
+console.log(newCake);
 
 
-    const pivot = array[array.length - 1]
-    const leftArr = [];
-    const rightArr = [];
-    for(let i = 0; i< array.length - 1; i++){
-        if(array[i] < pivot) {
-            leftArr.push(array[i]);
-        } else {
-            rightArr.push(array[i]);
+// quicksort algorithm, sorts on the last index of the array
+function quickSort(array) {
+
+    try {
+
+        if (array.length <= 1) {
+            return array;
         }
+
+        const pivot = array[array.length - 1]
+        const leftArr = [];
+        const rightArr = [];
+
+        for (const element of array.slice(0, array.length - 1)) {
+            element < pivot ? leftArr.push(element) : rightArr.push(element);
+        }
+
+        return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+
+    } catch (error) {
+        console.log("Error with quicksort function");
     }
 
-    if(leftArr.length > 0 && rightArr.length > 0) {
-        return[...quickSort(leftArr), pivot, ...quickSort(rightArr)]
-    } else if ( leftArr > 0) {
-        return[...quickSort(leftArr), pivot];
-    } else {
-        return[pivot, ...quickSort(rightArr)]
-    }
 }
 
 console.log(quickSort(toppings));
-
-const cakeWithTopping = toppings.map(quickSort(cakeMaker2("cake")));
-console.log(cakeWithTopping);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-
-  
-  
-  
-  
-
-
