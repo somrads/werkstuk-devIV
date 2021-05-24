bd4186efde44d136da44f68475cb599de01eeddd
somrads <43730541+somrads@users.noreply.github.com>
2021-05-24 11:40:53 +0200

    ghost code weggedaan

--- src/test/java/ehb/multec/werksuk/CakeMakerTest.java
+++ src/test/java/ehb/multec/werksuk/CakeMakerTest.java
@@ -4,11 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CakeMakerTest {
-<<<<<<< HEAD
-//template methode pattern unit test
-=======
-//template methode pattern unit test getest als feature
->>>>>>> feature/unit-testing
+
 
   @Test
   void makeCakes(){
@@ -29,11 +25,7 @@ class CakeMakerTest {
     plaincake.bakeInOven();
 
   }
-<<<<<<< HEAD
-//decorater pattern unit test
-=======
-//decorater pattern unit test gestest als feature
->>>>>>> feature/unit-testing
+
 @Test
 void addTopping(){
   Cake chocoCake = new Chocolate(new WhipCream(new PlainCake()));
